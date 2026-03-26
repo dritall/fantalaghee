c8de96ceeacbed6c00f3487fd6ea6a38edb68e93
dritall <86dari@gmail.com>
2026-03-26 22:10:52 +0100

    Add remote pattern for img.legaseriea.it and ui-avatars.com

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'api.sofascore.app' },
-      { protocol: 'https', hostname: 'images.fotmob.com' }
+      { protocol: 'https', hostname: 'images.fotmob.com' },
+      { protocol: 'https', hostname: 'img.legaseriea.it', pathname: '/vimages/**' },
+      { protocol: 'https', hostname: 'ui-avatars.com' },
     ],
   },
 }
